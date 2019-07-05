f5cf52ca2f45aa94bc2ca3b200fd94b1487c1399
DavidNatanael <davidstinghen@gmail.com>
2019-07-05 16:58:22 -0300

    fet: add method store in use controller

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,22 @@
+import User from '../models/User';
+
+class UserController {
+  /** 
+   * method to store a new user
+  */
+ async store(req, res) {
+   // check if user exists
+   const { email } = req.body
+   const userExists = await User.findOne({ where: { email } });
+   if (userExists) {
+     return res.status(400).json({ error: 'User alredy exists'})
+   } 
+
+   // if all is ok, store the user
+   const { id, name } = await User.create(req.body);
+   return res.json({ id, name, email });
+ }
+
+}
+
+export default new UserController();
