0f7c36615aba8c35e3169475e433fa31fa69c999
DavidNatanael <davidstinghen@gmail.com>
2019-07-07 13:16:44 -0300

    fet: add user controller

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,7 +1,7 @@
 import User from '../models/User';
 
 class UserController {
-  /** 
+  /**
    * method to store a new user
   */
  async store(req, res) {
@@ -10,7 +10,7 @@ class UserController {
    const userExists = await User.findOne({ where: { email } });
    if (userExists) {
      return res.status(400).json({ error: 'User alredy exists'})
-   } 
+   }
 
    // if all is ok, store the user
    const { id, name } = await User.create(req.body);
